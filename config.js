b2f6975fa23e4269003641d91d72cf2a17e19356
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-09-15 20:05:19 +0100

    Task finished

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '405728905073-68jnme700rodv9eg0020eg37qps8if6j.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'RPO1x__fu_9nCMlgvML2_QzV',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+
+};
